9f962a9a8ad56aab95e14e42bc815cc13d84dfa3
Aditi Jain <jaditi930@gmail.com>
2023-07-14 23:15:49 +0530

    contact search implemented

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import ContactCard from "./ContactCard";
 import {Link} from "react-router-dom";
 function ContactList(props){
-// console.log(props)
+const [searchValue,setSearchValue]=useState("")
+console.log(props)
 let contactsList;
-    if(props.contacts.length>0)
+    if(props.contacts.length>0&&searchValue==""){
     contactsList=props.contacts.map((contact)=>{
         return <ContactCard  contact={contact} deleteContactHandler={props.deleteContactHandler}/>
     })
+}
+    else{
+        contactsList=props.contacts.map((contact)=>{
+            let conc=contact.name+contact.phoneNumber;
+            console.log(conc);
+            if (conc.includes(searchValue))
+            return <ContactCard  contact={contact} deleteContactHandler={props.deleteContactHandler}/>
+        })
+    }
     return( <div>
+    <input type="text" name="search_bar" onChange={(e)=>setSearchValue(e.target.value)}></input>
     <ul>
     {contactsList}
     </ul>
